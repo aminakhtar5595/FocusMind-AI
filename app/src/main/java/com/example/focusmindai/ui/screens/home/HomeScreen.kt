bb4f28b1248c2ea5eb88c082a2831eb359f574b4
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-13 09:52:33 +0500

    Added menu icon in header in Home Screen

--- app/src/main/java/com/example/focusmindai/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/focusmindai/ui/screens/home/HomeScreen.kt
@@ -1,8 +1,35 @@
 package com.example.focusmindai.ui.screens.home
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
-
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+    ) {
+        Row (
+            modifier = Modifier.padding(bottom = 20.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = "Arrow Back",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clickable { },
+            )
+        }
+    }
 }
