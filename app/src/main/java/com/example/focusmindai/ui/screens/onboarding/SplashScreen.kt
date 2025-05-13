b23192734244893167571efa7dc4b3a075e39739
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-13 09:32:08 +0500

    Added internet permission in android manifest

--- app/src/main/java/com/example/focusmindai/ui/screens/onboarding/SplashScreen.kt
+++ app/src/main/java/com/example/focusmindai/ui/screens/onboarding/SplashScreen.kt
@@ -1,8 +1,24 @@
 package com.example.focusmindai.ui.screens.onboarding
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun SplashScreen() {
-
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+    }
 }
