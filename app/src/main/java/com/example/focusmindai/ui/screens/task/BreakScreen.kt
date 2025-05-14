c61d0fc8fbd30e83c6f651fed581bc7847ee7506
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-14 08:13:30 +0500

    Added short break box in break screen

--- app/src/main/java/com/example/focusmindai/ui/screens/task/BreakScreen.kt
+++ app/src/main/java/com/example/focusmindai/ui/screens/task/BreakScreen.kt
@@ -1,5 +1,6 @@
 package com.example.focusmindai.ui.screens.task
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,13 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -38,6 +42,26 @@ fun BreakScreen() {
                 text = "BREAK",
                 style = MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center),
                 modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Column (
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .border(width = 0.5.dp, color = Color.Black, shape = RoundedCornerShape(10))
+                .padding(15.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "5:00",
+                style = MaterialTheme.typography.displayLarge,
+                modifier = Modifier.padding(bottom = 5.dp)
+            )
+
+            Text(
+                text = "Short Break",
+                style = MaterialTheme.typography.titleLarge
             )
         }
     }
