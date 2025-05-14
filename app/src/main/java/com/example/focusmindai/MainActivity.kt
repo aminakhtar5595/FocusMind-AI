166aa2507501fca95b8ea0f498e6c403313cf77a
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-14 08:09:20 +0500

    Starts working on break screen

--- app/src/main/java/com/example/focusmindai/MainActivity.kt
+++ app/src/main/java/com/example/focusmindai/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.focusmindai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.focusmindai.ui.screens.home.HomeScreen
-import com.example.focusmindai.ui.screens.task.TaskSelectionScreen
+import com.example.focusmindai.ui.screens.task.BreakScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TaskSelectionScreen()
+            BreakScreen()
         }
     }
 }
