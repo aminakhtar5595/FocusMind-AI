f5c0e3e624a1333f05b1f3d39f0a7a0f094984af
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-14 07:26:48 +0500

    Added title time in first box in task selection screen

--- app/src/main/java/com/example/focusmindai/ui/screens/task/TaskSelectionScreen.kt
+++ app/src/main/java/com/example/focusmindai/ui/screens/task/TaskSelectionScreen.kt
@@ -1,5 +1,6 @@
 package com.example.focusmindai.ui.screens.task
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
@@ -14,7 +16,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -39,6 +43,21 @@ fun TaskSelectionScreen() {
                 text = "TASK SELECTION",
                 style = MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center),
                 modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Column (
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .border(width = 0.5.dp, color = Color.Black, shape = RoundedCornerShape(10))
+                .padding(15.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "25:00",
+                style = MaterialTheme.typography.displayLarge,
+                modifier = Modifier.padding(bottom = 5.dp)
             )
         }
     }
