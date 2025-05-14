7e904dd09b584d09e7c29cfdf423d39b8d5a2f49
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-14 07:21:42 +0500

    Starts working on task selection screen

--- app/src/main/java/com/example/focusmindai/MainActivity.kt
+++ app/src/main/java/com/example/focusmindai/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.focusmindai.ui.screens.home.HomeScreen
+import com.example.focusmindai.ui.screens.task.TaskSelectionScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            TaskSelectionScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/focusmindai/ui/screens/task/TaskSelectionScreen.kt
@@ -0,0 +1,8 @@
+package com.example.focusmindai.ui.screens.task
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskSelectionScreen() {
+
+}
