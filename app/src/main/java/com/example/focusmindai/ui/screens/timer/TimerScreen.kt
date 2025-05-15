95bcbc7b9f606e1df0b51bda319beca14f5ee58e
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-15 08:46:10 +0500

    Added check icon in first box in timer screen

--- app/src/main/java/com/example/focusmindai/ui/screens/timer/TimerScreen.kt
+++ app/src/main/java/com/example/focusmindai/ui/screens/timer/TimerScreen.kt
@@ -1,5 +1,6 @@
 package com.example.focusmindai.ui.screens.timer
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,13 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -39,6 +43,24 @@ fun TimerScreen() {
                 style = MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center),
                 modifier = Modifier.fillMaxWidth()
             )
+        }
+
+        Column (
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .border(width = 0.5.dp, color = Color.Black, shape = RoundedCornerShape(10))
+                .padding(15.dp)
+                .fillMaxWidth(),
+        ) {
+            Row {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = "Tick Icon",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clickable { }
+                )
+            }
         }
     }
 }
