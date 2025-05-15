348eedde2801c0503de05cb81c47928145569129
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-15 08:57:14 +0500

    Starts working on AI suggestions screen

--- app/src/main/java/com/example/focusmindai/MainActivity.kt
+++ app/src/main/java/com/example/focusmindai/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.focusmindai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.focusmindai.ui.screens.suggestions.SuggestionsScreen
 import com.example.focusmindai.ui.screens.task.BreakScreen
 import com.example.focusmindai.ui.screens.timer.TimerScreen
 
@@ -10,7 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimerScreen()
+            SuggestionsScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/focusmindai/ui/screens/suggestions/SuggestionsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.focusmindai.ui.screens.suggestions
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SuggestionsScreen() {
+
+}
